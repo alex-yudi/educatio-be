32e494d7d9b8a1c50dfd79fe79440a7b958dc1cc
Alex Yudi <yudi.toma@raidhut.com.br>
2025-06-29 15:24:54 -0300

    refactor: simplificar controlador e serviço removendo endpoints não utilizados
    - Corrigir formatação das propriedades ApiProperty no DTO de matrícula
    - Remover imports desnecessários no controlador de usuários
    - Remover métodos não utilizados no serviço de usuários
    - Manter apenas endpoints essenciais: login, cadastro de aluno, disciplina e matrícula

--- src/users/dto/create-matricula.dto.ts
+++ src/users/dto/create-matricula.dto.ts
@@ -2,17 +2,17 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateMatriculaDto {
-  @ApiProperty({ 
-    example: '2025001', 
-    description: 'Matrícula do aluno a ser matriculado' 
+  @ApiProperty({
+    example: '2025001',
+    description: 'Matrícula do aluno a ser matriculado'
   })
   @IsString()
   @IsNotEmpty()
   matricula_aluno: string;
 
-  @ApiProperty({ 
-    example: 'BD-2024-1A', 
-    description: 'Código da turma onde o aluno será matriculado' 
+  @ApiProperty({
+    example: 'BD-2024-1A',
+    description: 'Código da turma onde o aluno será matriculado'
   })
   @IsString()
   @IsNotEmpty()
